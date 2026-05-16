7a961902d06578f5bdd635555cdd834bd7cf1551
devikakanaparthi <devikakanaparthi96@gmail.com>
2026-05-16 12:33:09 +0530

    Create ani.py

---
+++ ani.py
@@ -0,0 +1,19 @@
+print("Simple chatbot")
+print("Type bye to stop")
+while True:
+    user=input("You:")
+    if user=="hello":
+        print("Bot:Hi!")
+    elif user=="how are you":
+        print("Bot: I am fine,thanks!")
+    elif user=="bye":
+        print("Bot: Goodbye!")
+        break
+    else:
+        print("Bot: sorry,I don't understand")          
+             
+            
+            
+            
+                
+          
